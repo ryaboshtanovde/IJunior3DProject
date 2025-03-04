6291dc5fce739a5e82a0c1831bca7ff6a2beed10
ryaboshtanovde <165268045+ryaboshtanovde@users.noreply.github.com>
2025-03-04 20:18:33 +0300

    Robber update

--- Assets/Scripts/Robber.cs
+++ Assets/Scripts/Robber.cs
@@ -1,11 +1,25 @@
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class Robber : MonoBehaviour
 {
     [SerializeField] private Transform _target;
     [SerializeField] private float _maxSpeed = 0.01f;
+    private bool _targetIsReceived = false;
+    private float _selectionDistance = 1;
+
     private void Update()
     {
-        transform.position = new Vector3(Mathf.MoveTowards(transform.position.x, _target.position.x, _maxSpeed), transform.position.y, transform.position.z);
+        if ((transform.position - _target.position).magnitude <= _selectionDistance)
+            _targetIsReceived = true;
+
+        if (_targetIsReceived)
+        {
+            transform.position = new Vector3(Mathf.MoveTowards(transform.position.x, _target.position.x, -_maxSpeed), transform.position.y, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(Mathf.MoveTowards(transform.position.x, _target.position.x, _maxSpeed), transform.position.y, transform.position.z);
+        }
     }
 }
